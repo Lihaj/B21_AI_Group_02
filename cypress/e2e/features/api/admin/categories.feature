7611bd5b4ae338271b39da3885e3375c4ad57e2b
Hirusha Fernando <heshanhfernando@gmail.com>
2026-05-29 16:58:23 +0530

    added api test case

--- cypress/e2e/features/api/admin/categories.feature
+++ cypress/e2e/features/api/admin/categories.feature
@@ -9,3 +9,8 @@ Feature: Admin - Categories API
     When I send a POST request to create category with fixture data "shortCategoryName"
     Then the response status should be 400
     And the response body should contain the error message "Category name must be between 3 and 10 characters"
+
+  @215523H @API_Category_Update_001
+  Scenario: Update category with valid data as Admin
+    When I send a PUT request to update category with fixture data "validCategoryUpdate"
+    Then the response status should be 200
--- cypress/e2e/step_definitions/api/categories.steps.ts
+++ cypress/e2e/step_definitions/api/categories.steps.ts
@@ -15,3 +15,18 @@ When('I send a POST request to create category with fixture data {string}', func
     })
   })
 })
+
+When('I send a PUT request to update category with fixture data {string}', function (fixtureKey: string) {
+  cy.get('@token').then((token) => {
+    cy.fixture('testdata').then((data) => {
+      const apiUrl = Cypress.env('apiUrl');
+      cy.request({
+        method: 'PUT',
+        url: `${apiUrl}/categories/${data[fixtureKey].id}`,
+        headers: { Authorization: `Bearer ${token}` },
+        body: data[fixtureKey],
+        failOnStatusCode: false
+      }).as('response')
+    })
+  })
+})
