82e7e8610f25c5dcabe14b66d376b02d3e397cf5
Hirusha Fernando <heshanhfernando@gmail.com>
2026-05-31 09:23:02 +0530

    refactor: update environment variable usage in Cypress configuration and workflow

--- cypress.config.js
+++ cypress.config.js
@@ -10,10 +10,10 @@ const { allureCypress } = require('allure-cypress/reporter');
 
 function queryDb(query, config) {
   return mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'test_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     multipleStatements: true,
   })
     .then((connection) => {
