3c767ed69c318b03e43a0a130301d8635920a168
Tharinduni Abeywickrama <tharinduni.abeywickrama0@gmail.com>
2026-06-09 19:30:19 +0530

    Add API test cases for plants

--- cypress/e2e/step_definitions/api/plants.steps.ts
+++ cypress/e2e/step_definitions/api/plants.steps.ts
@@ -1,5 +1,33 @@
 /// <reference types="cypress" />
-import { DataTable, Then, When } from '@badeball/cypress-cucumber-preprocessor'
+import { DataTable, Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
+
+Given('at least one plant exists', function () {
+	cy.get('@token').then((token) => {
+		cy.env(['apiUrl']).then(({ apiUrl }) => {
+			cy.request({
+				method: 'GET',
+				url: `${apiUrl}/plants`,
+				headers: { Authorization: `Bearer ${token}` },
+				failOnStatusCode: false
+			}).then((response) => {
+				if (Array.isArray(response.body) && response.body.length > 0) {
+					return
+				}
+				cy.request({
+					method: 'POST',
+					url: `${apiUrl}/plants/category/1`,
+					headers: { Authorization: `Bearer ${token}` },
+					body: {
+						name: 'Test Plant',
+						price: 10,
+						quantity: 10
+					},
+					failOnStatusCode: false
+				}).as('response')
+			})
+		})
+	})
+})
 
 When('I send a GET request to view plant with id {string}', function (plantId: string) {
 	cy.get('@token').then((token) => {
@@ -27,6 +55,19 @@ When('I send a GET request to search plants by name {string} page {string} size 
 	})
 })
 
+When('I send a GET request to retrieve all plants', function () {
+	cy.get('@token').then((token) => {
+		cy.env(['apiUrl']).then(({ apiUrl }) => {
+			cy.request({
+				method: 'GET',
+				url: `${apiUrl}/plants`,
+				headers: { Authorization: `Bearer ${token}` },
+				failOnStatusCode: false
+			}).as('response')
+		})
+	})
+})
+
 When('I send a POST request to create plant with plant data and category id {string}', function (categoryId: string, dataTable: DataTable) {
 	cy.get('@token').then((token) => {
 		cy.env(['apiUrl']).then(({ apiUrl }) => {
@@ -42,6 +83,51 @@ When('I send a POST request to create plant with plant data and category id {str
 				},
 				failOnStatusCode: false
 			}).as('response')
+		})
+	})
+})
+
+When('I send a PUT request to update plant with id {string} with plant data', function (plantId: string, dataTable: DataTable) {
+	cy.get('@token').then((token) => {
+		cy.env(['apiUrl']).then(({ apiUrl }) => {
+			const plantData = dataTable.hashes()[0]
+			cy.request({
+				method: 'PUT',
+				url: `${apiUrl}/plants/${plantId}`,
+				headers: { Authorization: `Bearer ${token}` },
+				body: {
+					name: plantData.name,
+					price: Number(plantData.price),
+					quantity: Number(plantData.quantity)
+				},
+				failOnStatusCode: false
+			}).as('response')
+		})
+	})
+})
+
+When('I send a DELETE request to delete plant with id {string}', function (plantId: string) {
+	cy.get('@token').then((token) => {
+		cy.env(['apiUrl']).then(({ apiUrl }) => {
+			cy.request({
+				method: 'DELETE',
+				url: `${apiUrl}/plants/${plantId}`,
+				headers: { Authorization: `Bearer ${token}` },
+				failOnStatusCode: false
+			}).as('response')
+		})
+	})
+})
+
+Then('the plant with id {string} should not exist', function (plantId: string) {
+	cy.get('@token').then((token) => {
+		cy.env(['apiUrl']).then(({ apiUrl }) => {
+			cy.request({
+				method: 'GET',
+				url: `${apiUrl}/plants/${plantId}`,
+				headers: { Authorization: `Bearer ${token}` },
+				failOnStatusCode: false
+			}).its('status').should('eq', 404)
 		})
 	})
 })
--- cypress/e2e/step_definitions/common/share.steps.ts
+++ cypress/e2e/step_definitions/common/share.steps.ts
@@ -10,3 +10,25 @@ Then('the response body should contain the error message {string}', (errorMessag
     expect(JSON.stringify(body)).to.include(errorMessage)
   })
 })
+
+Then('the response body should contain the success message {string}', (successMessage: string) => {
+  cy.get('@response').its('body').then((body) => {
+    expect(JSON.stringify(body)).to.include(successMessage)
+  })
+})
+
+Then('the response body should contain updated plant details', () => {
+  cy.get('@response').its('body').then((body) => {
+    expect(body).to.be.an('object')
+    expect(body).to.have.property('name')
+    expect(body).to.have.property('price')
+    expect(body).to.have.property('quantity')
+  })
+})
+
+Then('the response body should contain a non-empty plant list', () => {
+  cy.get('@response').its('body').then((body) => {
+    expect(body).to.be.an('array')
+    expect(body.length).to.be.greaterThan(0)
+  })
+})
